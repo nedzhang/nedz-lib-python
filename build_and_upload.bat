f16ebc08e209a5dba604801740bacd4b075d7f4d
Ned Zhang <ned_zhang@us.ibm.com>
2022-06-07 09:59:43 -0500

    Read token from a file.

--- build_and_upload.bat
+++ build_and_upload.bat
@@ -1,10 +1,17 @@
+@REM ***********************************************************
+@REM This script builds and uploads python lib to testpypi
+@REM ***********************************************************
+
+echo off
+
+@REM Read the token for pypi from file
+set /p PYPI_TOKEN=<"secret/token.txt"
+
 del /Q .\dist\*
 
 python -m build
 
-python -m twine upload --repository testpypi -u __token__ -p pypi-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX --verbose dist/* 
-
-echo off
+python -m twine upload --repository testpypi -u __token__ -p %PYPI_TOKEN% --verbose dist/* 
 
 echo ****************************************************************************
 echo *     Install the package with command below                               *
